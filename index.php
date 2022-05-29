cc73d6355d49eb3397306c3995c7d14b31124c43
kalilim269 <98217122+kalilim269@users.noreply.github.com>
2022-05-29 19:07:07 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,129 @@
+<?php
+include_once 'loginfunctions.php';
+if (!isLoggedIn()) {
+  $_SESSION['msg'] = "You must log in first";
+  header('location: login.php');
+}
+
+$f = fopen("temp.dat", "r+");
+if ($f !== false) {
+    ftruncate($f, 0);
+    fclose($f);
+}
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+   <title>EDU APP INTERFACE SELECTION : Home</title>
+   <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+ 
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+<style type="text/css">
+@import url(https://fonts.googleapis.com/css?family=Open+Sans);
+
+body {
+        width:100%;
+        height:100%;
+        background:url(starbooklogo.png) center center no-repeat;
+        min-height:100%;
+        background-color: #FFFFF0;
+        margin:  0;
+        font-family: "Lato", sans-serif;
+}
+
+.form-style-9{
+  max-width: 800px;
+  background: #FFFAF0;
+  padding: 50px;
+  padding-top: 10px;
+  margin: 50px auto;
+  border-radius: 10px;
+  border: 6px solid #F08080;
+}
+
+.sidebar {
+  margin: 20px;
+  padding: 10px;
+  width: 200px;
+  background-color: #FFE4E1;
+  position: absolute;
+  height: 69%;
+  overflow: auto;
+}
+
+.sidebar a {
+  display: block;
+  color: black;
+  padding: 16px;
+  text-decoration: none;
+  margin-bottom: 17px;
+}
+ 
+.sidebar a.active {
+  background-color: #FA8072;
+  color: white;
+}
+
+.sidebar a:hover:not(.active) {
+  background-color: #AFEEEE;
+  color: black;
+}
+
+div.content {
+  margin-left: 250px;
+  padding: 1px 16px;
+  height: 1000px;
+}
+
+@media screen and (max-width: 700px) {
+  .sidebar {
+    width: 92%;
+    height: auto;
+    position: relative;
+  }
+  .sidebar a {float: left;}
+  div.content {margin-left: 0;}
+}
+
+@media screen and (max-width: 400px) {
+  .sidebar a {
+    text-align: center;
+    float: none;
+  }
+}
+</style>
+  
+</head>
+<body>
+  <?php include_once 'navi_bar.php'; ?>
+
+       
+  <div class="sidebar">
+  <a href="criteria.php"><b>Criteria</b></a>
+  <a href="subcriteria.php"><b>Sub-Criteria</b></a>
+  <a href="alternative.php"><b>Alternative</b></a>
+  <a href="method.php"><b>AHP Method</b></a>
+  <a href="rank.php"><b>Random Forest Model</b></a>
+  <a href="report.php"><b>Summary Report</b></a>
+  <a href="save.php"><b>Save Project</b></a>
+</div>
+
+   <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+
+</body>
+</html>
